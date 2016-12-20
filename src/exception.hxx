1009f69b2d9bae2c2def2b0859efe537be7a30a8
Benoit Gschwind <gschwind@gnu-log.net>
2016-12-20 23:24:28 +0100

    add/import simple exception implementation

---
+++ src/exception.hxx
@@ -0,0 +1,56 @@
+/*
+ * Copyright (2014-2016) Benoit Gschwind
+ *
+ * This file is part of page-compositor.
+ *
+ * page-compositor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * page-compositor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with page-compositor.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EXCEPTION_HXX_
+#define EXCEPTION_HXX_
+
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <exception>
+
+namespace page {
+
+using namespace std;
+
+class except : public exception {
+	char * str;
+public:
+	except(char const * fmt, ...) : str(nullptr) {
+		va_list l;
+		va_start(l, fmt);
+		if(vasprintf(&str, fmt, l) < 0)
+			str = nullptr;
+		va_end(l);
+	}
+
+	~except() noexcept { free(str); }
+
+	char const * what() const noexcept {
+		return str?str:"nested memory error: could not allocate error message";
+	}
+
+};
+
+}
+
+
+
+#endif /* EXCEPTION_HXX_ */
